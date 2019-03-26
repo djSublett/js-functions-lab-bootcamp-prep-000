5021ce95fca36232bd8a0359abfcb38aaa56c3aa
Dale Sublett <djsublett04@gmail.com>
2019-03-26 20:28:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,16 @@ function happyHolidays() {
   return "Happy holidays!"
 }
 
-function happyHolidaysTo() {
-  return 
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
 }
+
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(days, holiday) {
+  return `It's ${days} days until ${holiday}!`
+}
+
 
